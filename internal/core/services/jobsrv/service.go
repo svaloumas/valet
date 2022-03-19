4aad60b96c9585dc0a7febe69e1e85ccb0ce7f5b
Stefanos Valoumas <svaloumas@gmail.com>
2022-03-19 20:14:24 +0200

    feat: add jobsrv service

---
+++ internal/core/services/jobsrv/service.go
@@ -0,0 +1,40 @@
+package jobsrv
+
+import (
+	"time"
+	"valet/internal/core/domain"
+	"valet/internal/core/ports"
+)
+
+type service struct {
+	jobRepository ports.JobRepository
+	// Implement this.
+	uuidGen string
+}
+
+func New() *service {
+	return &service{}
+}
+
+func (srv *service) Create(name, description string) error {
+	createdAt := time.Now()
+	job := &domain.Job{
+		ID:          srv.uuidGen,
+		Name:        name,
+		Description: description,
+		Status:      domain.Pending,
+		CreatedAt:   &createdAt,
+	}
+	if err := job.Validate(); err != nil {
+		return err
+	}
+	return srv.jobRepository.Create(job)
+}
+
+func (srv *service) Get(id string) (*domain.Job, error) {
+	return srv.jobRepository.Get(id)
+}
+
+func (srv *service) Delete(id string) error {
+	return srv.jobRepository.Delete(id)
+}
