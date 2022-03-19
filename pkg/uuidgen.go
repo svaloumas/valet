ca15254f8fa5b57a35e6b5f888f2861237345e94
Stefanos Valoumas <svaloumas@gmail.com>
2022-03-19 23:37:36 +0200

    feat: add uuidgen pkg

---
+++ pkg/uuidgen.go
@@ -0,0 +1,19 @@
+package uuidgen
+
+import (
+	"github.com/google/uuid"
+)
+
+type UUIDGenerator interface {
+	GenerateRandomUUIDString() (string, error)
+}
+
+type UUIDGen struct{}
+
+func (u *UUIDGen) GenerateRandomUUIDString() (string, error) {
+	uuid, err := uuid.NewRandom()
+	if err != nil {
+		return "", nil
+	}
+	return uuid.String(), nil
+}
