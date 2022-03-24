7e0ce3b47c0d2e8b50a1f136b37e98a6155dc8d7
Stefanos Valoumas <svaloumas@gmail.com>
2022-03-24 21:56:08 +0200

    feat: add the rest endpoints swagger doc

--- doc/swagger/job.go
+++ doc/swagger/job.go
@@ -8,12 +8,12 @@ import (
 // swagger:route POST /jobs jobs createJobs
 // Creates a new job.
 // responses:
-//   200: jobResponse
+//   202: postJobsResponse
 //   500: errorResponse
 
 // Returns the newly created job metadata.
-// swagger:response jobResponse
-type jobResponse struct {
+// swagger:response postJobsResponse
+type postJobsResponse struct {
 	// in:body
 	Body domain.Job
 }
@@ -23,4 +23,53 @@ type postJobsRequestParams struct {
 	// Name, description and metadata of the new job to be performed.
 	// in:body
 	Body jobhdl.RequestBodyDTO
+}
+
+// swagger:route GET /jobs/:id jobs getJob
+// Returns a specified job.
+// responses:
+//   200: getJobResponse
+//   404: errorResponse
+//   500: errorResponse
+
+// The specified job's metadata.
+// swagger:response getJobResponse
+type getJobResponse struct {
+	// in:body
+	Body domain.Job
+}
+
+// swagger:parameters getJobRequestParams getJob
+type getJobRequestParams struct {
+	// The ID of the specified job.
+	// in:path
+	id string `json:"id"`
+}
+
+// swagger:route PATCH /jobs/:id jobs patchJob
+// Updates a job's name of description.
+// responses:
+//   204:
+//   404: errorResponse
+//   500: errorResponse
+
+// swagger:parameters patchJobRequestParams patchJob
+type patchJobRequestParams struct {
+	// The ID of the specified job.
+	// in:path
+	id string `json:"id"`
+}
+
+// swagger:route DELETE /jobs/:id jobs deleteJob
+// Deletes a specified job.
+// responses:
+//   204:
+//   404: errorResponse
+//   500: errorResponse
+
+// swagger:parameters deleteJobRequestParams deleteJob
+type deleteJobRequestParams struct {
+	// The ID of the specified job.
+	// in:path
+	id string `json:"id"`
 }
---
+++ doc/swagger/result.go
@@ -0,0 +1,40 @@
+package swagger
+
+import (
+	"valet/internal/core/domain"
+)
+
+// swagger:route GET /jobs/:id/results jobs getJobResults
+// Returns a specified job's results.
+// responses:
+//   200: getJobResultsResponse
+//   404: errorResponse
+//   500: errorResponse
+
+// Returns a specified job results.
+// swagger:response getJobResultsResponse
+type getJobResultsResponse struct {
+	// in:body
+	Body domain.JobResult
+}
+
+// swagger:parameters getJobResultsRequestParams getJobResults
+type getJobResultsRequestParams struct {
+	// The ID of the specified job.
+	// in:path
+	id string `json:"id"`
+}
+
+// swagger:route DELETE /jobs/:id/results jobs deleteJobResults
+// Deletes a job's results.
+// responses:
+//   204:
+//   404: errorResponse
+//   500: errorResponse
+
+// swagger:parameters deleteJobResultsRequestsParams deleteJobResults
+type deleteJobResultsRequestsParams struct {
+	// The ID of the specified job.
+	// in:path
+	id string `json:"id"`
+}
